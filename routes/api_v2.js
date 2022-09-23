b4fe6e7623275a3a1bfd3cca98109c6f26dacee2
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2022-09-23 07:40:01 +0000

    [test]

--- routes/api_v2.js
+++ routes/api_v2.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const userManager = require('../lib/userManager');
 const fs = require('fs');
 const bcrypt = require("bcrypt");
